f3e5a7de0aff8e7b65b1772668dfa902b24d530a
Ryan <straightwind0726@gmail.com>
2023-11-08 16:52:23 +0900

    [Update] Generated the Header section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { styled } from 'styled-components';
 import './CSS/index.css';
+import Article from './Components/Article';
+import Footer from './Components/Footer';
+import Greeting from './Components/Greeting';
+import Header from './Components/Header';
+
+const MainOuter = styled.main`
+    padding: 1.5rem 0;
+`;
+
+const MainInner = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
 
 function App() {
-    return <div>hello, world</div>;
+    return (
+        <MainOuter>
+            <MainInner>
+                <Header />
+                <Greeting />
+                <Article />
+                <Footer />
+            </MainInner>
+        </MainOuter>
+    );
 }
 
 export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,99 @@
+import { styled } from 'styled-components';
+
+const HeaderOuter = styled.header`
+    margin-bottom: 2rem;
+    padding: 0 3rem;
+`;
+
+const HeaderInner = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const HeaderLogo = styled.div`
+    img {
+        width: 156px;
+        height: 75px;
+    }
+`;
+
+const HeaderMenu = styled.div`
+    ul {
+        display: flex;
+    }
+`;
+
+const MenuItem = styled.li`
+    padding: 0.5rem;
+    a {
+        text-decoration: none;
+        color: var(--primary-100);
+        font-size: 1.15rem;
+        font-weight: 500;
+        letter-spacing: -0.4px;
+    }
+    &:first-child {
+        text-decoration: underline var(--primary-100);
+        &:hover {
+            text-decoration: none;
+        }
+    }
+    &:hover {
+        text-decoration: underline var(--primary-100);
+    }
+`;
+
+export default function Header() {
+    const menuItem = [
+        {
+            id: 0,
+            url: 'https://www.anasantosilustracion.com/',
+            content: 'Home',
+        },
+        {
+            id: 1,
+            url: 'https://shop.anasantosilustracion.com/es/',
+            content: 'Shop',
+        },
+        {
+            id: 2,
+            url: 'https://shop.anasantosilustracion.com/es/contacta',
+            content: 'Contact',
+        },
+        {
+            id: 3,
+            url: 'https://www.domestika.org/es/courses/206-retrato-ilustrado-en-acuarela/anasantos',
+            content: 'Curso online/Domestika',
+        },
+        {
+            id: 4,
+            url: 'https://www.domestika.org/es/courses/482-tecnicas-experimentales-de-acuarela-para-principiantes/anasantos',
+            content: '2º Curso Domestika',
+        },
+    ];
+
+    return (
+        <HeaderOuter>
+            <HeaderInner>
+                <HeaderLogo>
+                    <img
+                        src="https://www.anasantosilustracion.com/contenido/uploads/2020/02/logo.jpg"
+                        alt="main logo"
+                    />
+                </HeaderLogo>
+                <HeaderMenu>
+                    <ul>
+                        {menuItem.map((item) => {
+                            return (
+                                <MenuItem key={item.id}>
+                                    <a href={item.url}>{item.content}</a>
+                                </MenuItem>
+                            );
+                        })}
+                    </ul>
+                </HeaderMenu>
+            </HeaderInner>
+        </HeaderOuter>
+    );
+}
