caac0313986d27c266845d473fa4452571d94ee6
Ryan <straightwind0726@gmail.com>
2023-11-09 12:29:42 +0900

    [Update] Generated the Greeting section

--- src/Components/Greeting.jsx
+++ src/Components/Greeting.jsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import { styled } from 'styled-components';
+
+const GreetingWrapper = styled.section`
+    width: 100%;
+    margin: 5rem 0;
+    div {
+        display: flex;
+        justify-content: center;
+        p {
+            font-size: 72px;
+            font-weight: 700;
+        }
+    }
+`;
 
 export default function Greeting() {
-    return <div>Greeting</div>;
+    return (
+        <GreetingWrapper>
+            <div>
+                <p>Ana Santos – Ilustración</p>
+            </div>
+        </GreetingWrapper>
+    );
 }
